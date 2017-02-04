129aeebb3df598f7fc9f5de457ad504f5fda272d
SawnOff <tylerf98@live.co.uk>
2017-02-04 18:04:19 +0000

    Creating ResponseProcessor.java

---
+++ Project/app/src/ResponseProcessor.java
@@ -0,0 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public final class ResponseProcessor {
+  
+  public static String respondToQ(String[] processed) {
+    // Create answer
+  }
+}
