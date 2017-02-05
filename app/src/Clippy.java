c31c333f6815b94f9b8a39ba201cb3067d8451b7
Michael Tsang <mct16@ic.ac.uk>
2017-02-05 09:43:19 +0000

    i want to die

--- app/src/Clippy.java
+++ app/src/Clippy.java
@@ -53,21 +53,23 @@ public class Clippy extends JFrame implements ActionListener {
     questionField.addActionListener(this);
     totalBox.add(questionField);
     
-    Border paddingBorder = BorderFactory.createEmptyBorder(10,10,10,10);
+    Border paddingBorder = BorderFactory.createEmptyBorder(30,30,30,30);
 
     this.answer = new JLabel("<html> Hi! I am Clippy, <br>" +
         "your Linux assistant. <br>" +
         "Would you like some assistance today? </html>");
-    Dimension dim = new Dimension(300, 300);
+    Dimension dim = new Dimension(300, 200);
     answer.setBorder(paddingBorder);
     answer.setMinimumSize(dim);
     answer.setPreferredSize(dim);
     answer.setMaximumSize(dim);
-    answer.setOpaque(true);
-    answer.setBackground(Color.yellow);
+    answer.setIcon(new ImageIcon("speech2.png"));
+    //answer.setOpaque(true);
+    //answer.setBackground(Color.YELLOW);
     answer.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+    answer.setHorizontalTextPosition(JLabel.CENTER);
     upperBox.add(answer);
-    
+
   }
   
   @Override
